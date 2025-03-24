838568ddc78a54289b3e52b94f9fcbccc0d29336
Rahul Rai <rairayrahul22@gmail.com>
2025-03-24 12:52:57 +0530

    Singleton

--- Program.cs
+++ Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is dev branch");
+            //Singleton s2 = Singleton.Getinstance;
+            //s2.Printdetails("From s2 second message");
+            //Singleton s1 = Singleton.Getinstance;
+            //s1.Printdetails("From s1");
+            //---------- by calling s1 and s2 we are getting count value 1 because we are using same instance
+
+
+            Parallel.Invoke( //it is used to call multiple methods at the same time to solve this we are gona use lock
+                () => Student1(),
+                () => Student2()
+                );
+
+        }
+
+        private static void Student2()
+        {
+            Singleton s2 = Singleton.Getinstance;
+            s2.Printdetails("From s2 second message");
+        }
+
+        private static void Student1()
+        {
+            Singleton s1 = Singleton.Getinstance;
+            s1.Printdetails("From s1");
         }
     }
 }
---
+++ Singleton.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern
+{
+    public sealed class Singleton // sealed class is used to prevent inheritance
+    {
+        private static int counter = 0; //used to how many constructor is called
+
+
+        //private static readonly object obj = new object(); //it is used to lock the object
+        //private static Singleton instance = null;
+        //--- public property is used to return only one instance of the class
+        //public static Singleton Getinstance
+        //{
+        //    get
+        //    {
+        //        lock (obj) //it run only one thread at a time
+        //        {
+        //            if (instance == null)
+        //            {
+        //                instance = new Singleton();
+        //            }
+        //        }
+
+        //        return instance;
+        //    }
+        //}
+
+
+        //private static readonly Singleton instance = new Singleton(); //eager loading  Instance is created at class loading time
+        //public static Singleton Getinstance
+        //{
+        //    get
+        //    {
+        //        return instance;
+        //    }
+        //}
+
+        private static readonly Lazy<Singleton> instance = new Lazy<Singleton>(() => new Singleton());
+        public static Singleton Getinstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+
+        public void Printdetails(string message) //public
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
